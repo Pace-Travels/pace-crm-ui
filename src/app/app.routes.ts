8f6bd95b531e483900a3452c6fb1d87e0e0fdd3a
Nagesh77777 <nagesh@pacetravels.in>
2026-08-03 19:15:58 +0530

    created header, footer and main layout component added and made a design for this code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './layouts/main-layout/main-layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+            {
+                path: '',
+                component: Dashboard
+            }
+        ]
+    }
+
+];
---
+++ src/app/shared/sidebar/sidebar.ts
@@ -0,0 +1,74 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.scss',
+})
+export class Sidebar implements OnInit {
+
+  collapsed = signal(false);
+
+  mobileOpen = signal(false);
+
+  isMobile = window.innerWidth < 992;
+
+  menu = [
+    { icon: 'fa-solid fa-house', title: 'Dashboard', route: '/' },
+    { icon: 'fa-solid fa-comments', title: 'Live Chat', route: '/chat' },
+    { icon: 'fa-solid fa-address-book', title: 'Contacts', route: '/contacts' },
+    { icon: 'fa-solid fa-bullhorn', title: 'Campaigns', route: '/campaigns' },
+    { icon: 'fa-solid fa-robot', title: 'AI Manager', route: '/ai' },
+    { icon: 'fa-solid fa-chart-column', title: 'Analytics', route: '/analytics' },
+    { icon: 'fa-solid fa-gear', title: 'Settings', route: '/settings' }
+  ];
+
+  ngOnInit(): void {
+
+    const state = localStorage.getItem('sidebar-collapsed');
+
+    if (state) {
+      this.collapsed.set(JSON.parse(state));
+    }
+
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+
+    this.isMobile = window.innerWidth < 992;
+
+    if (!this.isMobile) {
+      this.mobileOpen.set(false);
+    }
+
+  }
+
+  toggleSidebar() {
+
+    if (this.isMobile) {
+
+      this.mobileOpen.update(v => !v);
+
+      return;
+
+    }
+
+    this.collapsed.update(v => {
+
+      localStorage.setItem(
+        'sidebar-collapsed',
+        JSON.stringify(!v)
+      );
+
+      return !v;
+
+    });
+
+  }
+
+}
