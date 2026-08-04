237e66da4e4a108c2f2b818e4343a5a13e59cef1
Shadab Khatib <shadab.khatib@gmail.com>
2026-08-04 18:56:21 +0530

    feat: add auth module views

---
+++ src/app/features/auth/login-view/login-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-view.html',
+  styleUrl: './login-view.scss',
+})
+export class LoginView {}
