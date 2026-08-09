704f2d698552887c63e9c86e93129b9bf3542af8
Shadab Khatib <shadab.khatib@gmail.com>
2026-08-09 19:40:57 +0530

    chore: update facebookAppId in production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://messengerapi.quotedesks.com/api/v1',
-  facebookAppId: '109841289150123'
+  facebookAppId: '647915904409904'
 };
