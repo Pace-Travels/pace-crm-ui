c4fadd075ef37ec57b725754fe324edb9c789ed3
Nagesh77777 <nagesh@pacetravels.in>
2026-08-18 11:23:16 +0530

    firebase configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,14 @@ export const environment = {
   apiUrl: 'http://localhost:3000/api/v1',
   // Pulled dynamically at runtime from backend .env via GET /api/v1/config/public
   facebookAppId: '647915904409904',
+
+  firebase: {
+    apiKey: "AIzaSyCHJ_KFIuSFrk2ytXoIysz15np5MsfLook",
+    authDomain: "pace-crm-fadc8.firebaseapp.com",
+    projectId: "pace-crm-fadc8",
+    storageBucket: "pace-crm-fadc8.firebasestorage.app",
+    messagingSenderId: "846502996207",
+    appId: "1:846502996207:web:1c7f7f15e506ac8803131a",
+    measurementId: "G-V0VRT7SMNS"
+  }
 };
